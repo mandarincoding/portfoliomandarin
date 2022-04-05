63dffdc286d18ccde0e8d1eb6ed01c9c18e5bfdf
mandarincoding <96864299+mandarincoding@users.noreply.github.com>
2022-04-05 14:23:16 +0900

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -11,6 +11,20 @@ $(function () {
     } else if (Math.abs(rota % 360) == 90) {
       $(".page1").css("opacity", "0");
       $(".page2").css("opacity", "1");
+      $(".page2").html('<div class="movingText testMove1">KIM GYURI</div>');
+      $(".page2").append('<div class="movingText testMove2">1996.05.16</div>');
+      $(".page2").append(
+        '<div class="movingText testMove3">ISFP</div>'
+      );
+      $(".page2").append(
+        '<div class="movingText testMove4">mandarincoding@gmail.com</div>'
+      );
+      $(".page2").append('<div class="movingText testMove5">010.5913.7294</div>');
+      $(".testMove1").animate({ right: "100" }, 1500);
+      $(".testMove2").delay(1000).animate({ right: "100" }, 1500);
+      $(".testMove3").delay(2000).animate({ right: "100" }, 1500);
+      $(".testMove4").delay(3000).animate({ right: "100" }, 1500);
+      $(".testMove5").delay(4000).animate({ right: "100" }, 1500);
       $(".page3").css("opacity", "0");
       $(".page4").css("opacity", "0"); 
     } else if (Math.abs(rota % 360) == 180) {
@@ -18,12 +32,25 @@ $(function () {
       $(".page2").css("opacity", "0");
       $(".page3").css("opacity", "1");
       $(".page4").css("opacity", "0"); 
+       $(".page3").html('<div class="htmlName">html5</div><div class="skillAll skill1"></div>');
+      $(".page3").append('<div class="jspName">javascript</div><div class="skillAll skill2"></div>');
+      $(".page3").append('<div class="cssName">css</div><div class="skillAll skill3"></div>');
+      $(".page3").append('<div class="jqueryName">jquery</div><div class="skillAll skill4"></div>');
+      $(".page3").append('<div class="photoName">photoshop</div><div class="skillAll skill5"></div>');
+      $(".skill1").animate({ width: "90%" }, 1500);
+      $(".skill2").delay(1000).animate({ width: "60%" }, 1500);
+      $(".skill3").delay(2000).animate({ width: "90%" }, 1500);
+      $(".skill4").delay(3000).animate({ width: "90%" }, 1500);
+      $(".skill5").delay(4000).animate({ width: "50%" }, 1500);
     } else if (Math.abs(rota % 360) == 270) {
       // 270도
       $(".page1").css("opacity", "0");
       $(".page2").css("opacity", "0");
       $(".page3").css("opacity", "0");
       $(".page4").css("opacity", "1");
+      $(".page4").html('<div class="go1"><a href="http://mandarincoding7.dothome.co.kr/power"><img  class="popUp1" src="./image/캐릭터소개.jpg" alt="파워퍼프걸"></a></div>');
+      $(".page4").append('<div class="go2"><a href="https://mandarincoding.github.io/alive/"><img class="popUp2" src="./image/얼라이브.jpg" alt="얼라이브"></a></div>');
+      $(".page4").append('<div class="go3"><a href="https://mandarincoding.github.io/cakehouse/"><img class="popUp3" src="./image/케이크하우스.jpg" alt="케이크하우스"></a></div>');
     }
   });
   $(".downBtn").on("click", function () {
